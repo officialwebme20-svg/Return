ce16767ce39d1c177dd539fda837c84d8ac488a1
officialwebme20-svg <officialwebme2.0@gmail.com>
2026-09-24 05:06:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post("/api/subscribe", async (req, res) => {
 
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
